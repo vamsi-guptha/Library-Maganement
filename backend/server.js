6739d9db34ecdf1d377184f3c885a8f6d6ebfd9b
Vamsi Makam <vamsimakam@Vamsis-MacBook-Air.local>
2026-04-08 22:23:12 +0530

    Configure Vercel API with new experimentalServices spec

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,6 @@ const PORT = process.env.PORT || 5001;
 // Seed data
 seedData();
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
